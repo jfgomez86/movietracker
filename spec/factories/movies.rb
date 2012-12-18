66c87707cb88700472b8bae14b9b1547feee84ff
Hernan Orozco <horozco15@gmail.com>
2012-12-18 17:58:23 -0500

    factories and testing for models created

--- spec/factories/movies.rb
+++ spec/factories/movies.rb
@@ -1,6 +1,8 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :movie do
+    title "example title"
+    synopsis "this is a synopsis example"
+    release_date "11/05/2012"
+    cover "http://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Factory_girl.jpg/220px-Factory_girl.jpg"
   end
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe Movie do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it {should validate_presence_of(:title)}
+  it {should validate_presence_of(:synopsis)}
+  it {should validate_presence_of(:release_date)}
 end
