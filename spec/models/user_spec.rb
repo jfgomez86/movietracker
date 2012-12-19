91704a64d9ee9292130858d53ca234377b0667af
Jose Felix Gomez <jfgomez86@gmail.com>
2012-12-19 09:24:53 -0500

    Removing unused specs from user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,4 @@
 require 'spec_helper'
 
 describe User do
-  it {should validate_presence_of(:full_name)}
-  it {should validate_presence_of(:email)}
-  it {should validate_presence_of(:password)}
-  it {should validate_uniqueness_of(:email)}
 end
