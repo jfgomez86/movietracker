8a6fc73239da2f32a7e6b44e2e552cdac6ce74c1
Edgar Ortega Ramírez <EdgarOrtegaRamirez@outlook.com>
2012-12-20 15:29:32 -0500

    Movie show page shows the movie information
    - the movie show page shows movie information
    - added Movie model validations and validations specs

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,7 @@
 class Movie < ActiveRecord::Base
   attr_accessible :title, :synopsis, :release_date
+  validates :title, presence: true
+  validates :release_date, presence: true
 
   has_many :checkins
   has_many :users, through: :checkins
