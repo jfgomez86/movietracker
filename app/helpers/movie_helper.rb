b625944b4133bb8bda3d0565d25d07546823a6c8
Edgar Ortega Ramírez <EdgarOrtegaRamirez@outlook.com>
2012-12-21 17:28:27 -0500

    added search-by-synopsis and displayed movie poster on Movie show page and index page(thumbnail)
    - Added search-by-synopsis functionality
    - Movie show page shows the movie poster if exists.
    - Movie index, watchlist and search shows a thumbnail of the movie
    poster.

---
+++ app/helpers/movie_helper.rb
@@ -0,0 +1,5 @@
+module MovieHelper
+  def search_types
+    ['title', 'synopsis']
+  end
+end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ActiveRecord::Base
-  attr_accessible :title, :synopsis, :release_date
+  attr_accessible :title, :synopsis, :release_date, :poster
   validates :title, presence: true
   validates :release_date, presence: true
 
@@ -7,16 +7,16 @@ class Movie < ActiveRecord::Base
   has_many :users, through: :checkins
 
   has_attached_file :poster,
-            :styles => { :medium => "256x320>", :thumb => "128x140>" },
+            :styles => { :medium => "256x320>", :thumb => "60x80>" },
             :url => "/assets/posters/:id/:style/:basename.:extension",
             :path => ":rails_root/public/assets/posters/:id/:style/:basename.:extension"
   validates_attachment :poster,
             :content_type => { :content_type => ["image/jpg", "image/jpeg", "image/png"]  },
             :size => { :less_than => 3.megabytes }
 
-  def self.search(search)
+  def self.search(search, type)
     if search
-      find(:all, :conditions => ['title LIKE ?', "%#{search}%"])
+      find(:all, conditions: ["#{type} LIKE ?", "%#{search}%"])
     else
       find(:all)
     end
