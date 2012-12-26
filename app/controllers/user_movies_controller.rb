0a13ae42facd021f1633404fd3e5e1ba293661c1
Hernan Orozco <horozco15@gmail.com>
2012-12-26 17:32:18 -0500

    Added new functionality: A user is now able to watch the others users's watchlists.
    Added new users_controller to show the users list.
    Refactored user_movies_controller to show the watchlists.
    Refactored Routes for news controllers.
    Added and refactored test for controllers.

--- app/controllers/user_movies_controller.rb
+++ app/controllers/user_movies_controller.rb
@@ -2,6 +2,7 @@ class UserMoviesController < ApplicationController
   before_filter :authenticate_user!
   
   def index
-    @watched_movies = current_user.movies
+    @user = User.find(params[:id])
+    @watchlist_movies = @user.movies
   end
 end
