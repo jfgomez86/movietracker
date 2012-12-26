fd5e17c8c165d23cf9b0dd889c331cd963af4d34
Hernan Orozco <horozco15@gmail.com>
2012-12-26 11:22:39 -0500

    Customized admin show view.

--- app/admin/movies.rb
+++ app/admin/movies.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register Movie do
     f.inputs "Movie", :multipart => true do
       f.input :title
       f.input :synopsis
-      f.input :release_date, as: :date
+      f.input :release_date, as: :date, start_year: 1970, end_year: 2014
       f.input :poster, :as => :file,
                        :hint => f.template.image_tag(f.object.poster.url(:medium))
     end
@@ -14,9 +14,20 @@ ActiveAdmin.register Movie do
     column :synopsis, as: :text
     column :release_date
     column "Poster" do |movie| 
-        link_to image_tag("/assets/posters/#{movie.id}/thumb/#{movie.poster_file_name}",
+        link_to image_tag("/assets/posters/#{movie.id}/medium/#{movie.poster_file_name}",
                            :alt => "poster"), admin_movie_path(movie)
     end
     default_actions
+  end
+  show do
+    attributes_table do
+      row :title
+      row :synopsis
+      row :release_date
+      row :posters do
+        image_tag(movie.poster.url(:medium))
+      end
+    end
+    active_admin_comments
   end
 end
