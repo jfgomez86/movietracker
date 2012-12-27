789539b1270a26cd91cffc8fe6762e871c311831
kevin cifuentes <cifuenteskevin85@gmail.com>
2012-12-27 15:48:17 -0500

    added test for checkin model

--- spec/models/checkin_spec.rb
+++ spec/models/checkin_spec.rb
@@ -10,4 +10,19 @@ describe Checkin, "Validations" do
   it { should validate_presence_of(:user_id) }
   it { should validate_presence_of(:movie_id) }
   it { should validate_uniqueness_of(:movie_id).scoped_to(:user_id) }
+  
+  it "should return a hash not null with list of top five more checkin" do
+    FactoryGirl.create(:checkin)
+    Checkin.top_5.should_not be_nil
+  end
+
+  it "should return a hash with list of top five more checkin empty" do
+    Checkin.top_5.should be_empty
+  end
+
+  it "should return a hash with list of top five more checkin" do
+    a = FactoryGirl.create(:movie)
+    b = FactoryGirl.create(:checkin, movie_id: a.id)
+    Checkin.top_5.should eq({a.id => 1})
+  end
 end
